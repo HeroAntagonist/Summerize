47e4180e4f58ab9792fb6ec8de29a9867868845c
tomh <thomaschackett@gmail.com>
2023-12-07 20:18:46 -0500

    v1 - mvp

---
+++ Summerize.py
@@ -0,0 +1,44 @@
+from langchain.document_loaders import WebBaseLoader
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.summarize import load_summarize_chain
+from dotenv import load_dotenv
+import time
+import random 
+
+load_dotenv()
+
+def type_out(message):
+    for char in message:
+        rand_Num = random.uniform(.005,.05)
+        time.sleep(rand_Num)
+        yield char
+
+def summerize_web(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo-16k')
+    chain = load_summarize_chain(llm, chain_type="stuff", verbose=False)
+    return (chain.run(docs))
+
+def summerize_page():
+    try:
+        [print(x,end="", flush=True) for x in type_out("Please enter a url... ")]
+        for i in type_out(summerize_web(input())):
+            print(i, end="", flush=True)
+        print(' ')
+        print(' ')
+    except:
+        print('The URL you entered does not appear to be valid')
+
+while True:
+    summerize_page()
+    user_input = input("Do you want to summerize another webpage (yes/no):")
+    if user_input.lower() not in ('yes', 'y'):
+        print("Exiting the script...")
+        break  # Exit the loop, which ends the script
+
+
+
+
+
